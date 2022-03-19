6013cbb72089a6a25dd6986308c67032f572ff3b
zeesa1802 <zeesa18.2@gmail.com>
2022-03-19 14:19:46 +0500

    added sign up, login and sign out

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
+
+devise_for :users, controllers: { registrations: "registrations" }
+root 'home#index'
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end

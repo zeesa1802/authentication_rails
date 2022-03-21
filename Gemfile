4d510e63ee1124562dc4085994468464baa9220b
zeesa1802 <zeesa18.2@gmail.com>
2022-03-21 10:48:40 +0500

    added user policy using pundit

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.0"
 gem 'devise'
-
+gem "pundit"
 gem "rolify"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,20 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    @user.has_any_role? :admin, :newuser
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    @user.has_role? :admin
+  end
+end
